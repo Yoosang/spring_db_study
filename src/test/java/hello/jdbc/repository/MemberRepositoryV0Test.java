ee275c273c05775015a8723c69de75f6bd30a5ea
usang <usang6325@gmail.com>
2022-12-25 15:01:06 +0900

    JDBC 개발 조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,16 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -15,6 +19,10 @@ class MemberRepositoryV0Test {
     void crud() throws SQLException {
         Member member = new Member("memberV1", 10000);
         repository.save(member);
+
+        Member findMember = repository.findById("memberV1");
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
